d1c7ff747dcbc668ea2c0c690b438885460e6308
Jean-Louis GUENEGO <jlguenego@gmail.com>
2023-08-25 17:13:22 +0200

    build: gh-pages

--- src/components/user/UserConnectButton.vue
+++ src/components/user/UserConnectButton.vue
@@ -6,7 +6,9 @@ const link = ref("");
 
 onMounted(async () => {
   console.log("window.location.origin: ", window.location.origin);
-  const redirectUri = `${window.location.origin}/#/exchange_token`;
+  console.log("import.meta.env.BASE_URL: ", import.meta.env.BASE_URL);
+  const url = window.location.origin + import.meta.env.BASE_URL;
+  const redirectUri = `${url}#/exchange_token`;
   const stravaUri = "http://www.strava.com/oauth/authorize";
 
   const apiStore = useApiStore();
